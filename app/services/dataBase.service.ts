c7377ec96e36c961f88897c4dbec3843b5fb80b4
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-22 15:49:05 +0200

    try create dataBase.service.ts

--- app/services/dataBase.service.ts
+++ app/services/dataBase.service.ts
@@ -4,44 +4,61 @@ import { Observable as RxObservable } from "rxjs/Rx";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/do";
 
+// data in object
+import { UserModel } from "./../models/user.model";
+
 @Injectable()
 export class DataBaseService {
-    private serverUrl = "http://superquiz.apphb.com/api/";
-
-    constructor(private http: Http) { }
-
-    postData(data: any) {
-        let options = this.createRequestOptions();
-        return this.http.post(this.serverUrl, { data }, options)
-            .map(res => res.json());
+    dates = [];
+    constructor(){
+        
     }
 
-    getData() {
-        let headers = this.createRequestHeader();
-        return this.http.get(this.serverUrl, { headers: headers })
-            .map(res => res.json());
+    postData(data: UserModel){
+        this.dates.push(data);
     }
+
+    getData(id: number){
+        return this.dates.slice(id, 1);
+    }
+
+    // // web server
+    // private serverUrl = "http://superquiz.apphb.com/api/";
+
+    // constructor(private http: Http) { }
+
+    // postData(data: any) {
+    //     let options = this.createRequestOptions();
+    //     return this.http.post(this.serverUrl, { data }, options)
+    //         .map(res => res.json());
+    // }
+
+    // getData() {
+    //     let headers = this.createRequestHeader();
+    //     return this.http.get(this.serverUrl, { headers: headers })
+    //         .map(res => res.json());
+    // }
     
-    getResponseInfo() {
-        let headers = this.createRequestHeader();
-        return this.http.get(this.serverUrl, { headers: headers })
-            .do(res => res);
-    }
+    // getResponseInfo() {
+    //     let headers = this.createRequestHeader();
+    //     return this.http.get(this.serverUrl, { headers: headers })
+    //         .do(res => res);
+    // }
 
-    private createRequestOptions() {
-        let headers = new Headers();
-        headers.append("AuthKey", "my-key");
-        headers.append("AuthToken", "my-token");
-        headers.append("Content-Type", "application/json");
-        let options = new RequestOptions({ headers: headers });
-        return options;
-    }
-    private createRequestHeader() {
-        let headers = new Headers();
-        // set headers here e.g.
-        headers.append("AuthKey", "my-key");
-        headers.append("AuthToken", "my-token");
-        headers.append("Content-Type", "application/json");
-        return headers;
-    }
+    // private createRequestOptions() {
+    //     let headers = new Headers();
+    //     headers.append("AuthKey", "my-key");
+    //     headers.append("AuthToken", "my-token");
+    //     headers.append("Content-Type", "application/json");
+    //     let options = new RequestOptions({ headers: headers });
+    //     return options;
+    // }
+    // private createRequestHeader() {
+    //     let headers = new Headers();
+    //     // set headers here e.g.
+    //     headers.append("AuthKey", "my-key");
+    //     headers.append("AuthToken", "my-token");
+    //     headers.append("Content-Type", "application/json");
+    //     return headers;
+    // }
 }
--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from "@angular/core";
 
 import { UserModel } from "./../models/user.model";
+import { DataBaseService } from './../services/dataBase.service';
 
 @Injectable()
 export class UserService {
+    db: DataBaseService;
   register(user: UserModel){
+      this.db.postData(user)
       console.log("User is login!");
+  }
+
+  getUser(id: any){
+      return this.db.getData(id);
   }
 }
