d0ceecc04a3d4fb82fb258a765c5c3dbf9fe5098
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-22 12:27:01 +0200

    Add in app.module.js forms

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 
 import { AppComponent } from "./app.component";
@@ -12,6 +13,7 @@ import { appComponents, appRoutes } from "./app.routing";
     declarations: [AppComponent, ...appComponents],
     bootstrap: [AppComponent],
     imports: [NativeScriptModule,
+              NativeScriptFormsModule,
               SlidesModule,
               NativeScriptRouterModule,
               NativeScriptRouterModule.forRoot(appRoutes)],
--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -3,7 +3,13 @@ import { Component } from "@angular/core";
 @Component({
     selector: "login",
     templateUrl: "./components/login/login.component.html",
+    styleUrls: ["components/login/login.css"]
 })
 export class LoginComponent { 
     public constructor() {}
+
+    public submit(){
+        alert("hello");
+    }
 }
+
